12c8151de8d3324ce30f4d3b52b961f413ea9b52
Lofty Razani <38493595+loftyr@users.noreply.github.com>
2019-02-06 16:37:36 +0700

    Add files via upload

---
+++ application/views/beranda_v.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('plugin/bootstrap/css/bootstrap.css') ?>">
+    <!-- Sweet Alert 2 CSS -->
+    <link rel="stylesheet" href="<?= base_url('plugin/sweetalert2.min.css') ?>">
+    <!-- My Style CSS -->
+    <link rel="stylesheet" href="<?= base_url('plugin/MyStyle.css') ?>">
+
+    <title>Welcome To Job Medan</title>
+  </head>
+  <body>
+    
+    <div class="container bg-container">
+
+    	<!-- Navbar -->
+    	<div class="navigasi">
+		 	<ul class="nav justify-content-end">
+				<li class="nav-item">
+			    	<a class="nav-link aktif" href="#">Home</a>
+			  	</li>
+			  	<li class="nav-item">
+			    	<a class="nav-link" href="#">Tentang</a>
+			  	</li>
+			  	<li class="nav-item">
+			    	<a class="nav-link" href="#">Follow US</a>
+			  	</li>
+			</ul>
+		</div>
+    	<!-- Akhir Navbar -->
+    	
+    	<div class="main-body">
+    		
+    	</div>
+    </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?= base_url('plugin/jquery-3.3.1.min.js') ?>"></script>
+    <script src="<?= base_url('plugin/popper.min.js') ?>"></script>
+    <script src="<?= base_url('plugin/bootstrap/js/bootstrap.min.js') ?>"></script>
+  </body>
+</html>
